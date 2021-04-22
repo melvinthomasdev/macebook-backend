fe5f5577d91f2ef6de90b03c65af1559ac637de0
Sruthy <sruthyml328@gmail.com>
2021-04-23 01:20:22 +0530

    add swagger and logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger} from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { config } from 'dotenv';
+
+config();
 
 async function bootstrap() {
+  const logger = new Logger('Macebook Server');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const swaggerOptions = new DocumentBuilder()
+      .setTitle('Macebook Server')
+      .setDescription('TnP MACE')
+      .setVersion('1.0.0')
+      .addTag('TnP MACE')
+      .addBearerAuth()
+      // .addServer('http://')
+      .build();
+
+    const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
+    SwaggerModule.setup('api-doc', app, swaggerDocument);
+
+    const port = process.env.PORT || 4009;
+    await app.listen(port);
+    logger.log(`Application Listening on Port ${port} `);
+    logger.log(`Api documentation available at "/api-doc/`);
 }
 bootstrap();
